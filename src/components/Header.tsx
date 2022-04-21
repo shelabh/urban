0aa69e446519943e2c6b303288e4e748f4e95ce3
shelabh <68127067+shelabh@users.noreply.github.com>
2022-04-21 15:23:09 +0530

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+const Header = () => (
+	<div className="w-full z-10 ">
+		<div className="w-full flex justify-around pt-10  items-center">
+			<div className="flex space-x-20 font-Titillium z-50 ">
+				<div className="cursor-pointer">
+					Explore
+				</div>
+				<div className="cursor-pointer">
+					Activity
+				</div>
+				<div className="cursor-pointer">
+					How it works
+				</div>
+			</div>
+		</div>
+	</div>
+)
+
+export default Header
